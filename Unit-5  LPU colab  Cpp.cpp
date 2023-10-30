689211059fe0d6541ad0c87d28d8b162ace2c90a
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2023-10-31 00:22:24 +0530

    Update Unit-5  LPU colab  Cpp.cpp

--- Unit-5  LPU colab  Cpp.cpp
+++ Unit-5  LPU colab  Cpp.cpp
@@ -411,3 +411,55 @@ int main() {
 //Output 2 :
 //Edit distance between intention and execution is 5
 
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class EditDistanceCalculator {
+public:
+    virtual int min(int x, int y, int z) = 0;
+};
+
+class MinEditDistanceCalculator : public EditDistanceCalculator {
+public:
+    int min(int x, int y, int z) override {
+        return std::min(std::min(x, y), z);
+    }
+
+    int minEditDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        int dp[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0)
+                    dp[i][j] = j;
+                else if (j == 0)
+                    dp[i][j] = i;
+                else if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+
+        return dp[m][n];
+    }
+};
+
+int main() {
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    MinEditDistanceCalculator calculator;
+    int distance = calculator.minEditDistance(str1, str2);
+
+    cout << "Edit distance between " << str1 << " and " << str2 << " is " << distance << endl;
+
+    return 0;
+}
+
+
